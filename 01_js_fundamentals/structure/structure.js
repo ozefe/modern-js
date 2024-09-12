abe206bce41b53e74fecc4100b8eb7f74939ccd5
ozefe <gh@efe.cv>
2024-09-12 21:43:05 +0300

    Add fundamental code structure introduction
    Signed-off-by: ozefe <gh@efe.cv>

---
+++ 01_js_fundamentals/structure/structure.js
@@ -0,0 +1,8 @@
+/* An example with two messages.
+This is a multiline comment.
+*/
+
+// This comment occupies a line of its own
+alert("Hello");
+
+[1, 2].forEach(alert); // This comment follows the statement
