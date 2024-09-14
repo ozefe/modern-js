26fb55c7473941f464d7bd2635921ff34b49e29e
Efe Özyay <gh@efe.cv>
2024-09-14 15:49:42 +0300

    Add coding style exercises
    Signed-off-by: Efe Özyay <gh@efe.cv>

---
+++ 02_code_quality/02_coding-style/coding-style.js
@@ -0,0 +1,54 @@
+function pow(x, n) {
+  let result = 1;
+
+  for (let i = 0; i < n; i++) {
+    result *= x;
+  }
+
+  return result;
+}
+
+let x = prompt("x?", "");
+let n = prompt("n?", "");
+
+if (n < 0) {
+  alert(`Power ${n} is not supported,
+        please enter a non-negative integer number`);
+} else {
+  alert(pow(x, n));
+}
+
+let str = `
+  ECMA International's TC39 is a group of JavaScript developers,
+  implementers, academics, and more, collaborating with the community
+  to maintain and evolve the definition of JavaScript.
+`;
+
+function pow(x, n) {
+  if (n < 0) {
+    alert("Negative 'n' not supported");
+  } else {
+    let result = 1;
+
+    for (let i = 0; i < n; i++) {
+      result *= x;
+    }
+
+    return result;
+  }
+}
+
+function pow(x, n) {
+  if (n < 0) {
+    alert("Negative 'n' not supported");
+    return;
+  }
+
+  let result = 1;
+
+  for (let i = 0; i < n; i++) {
+    result *= x;
+  }
+
+  return result;
+}
