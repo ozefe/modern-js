5db3cf035444042a3df4ac4b4c93e4d53d41257e
Efe Özyay <gh@efe.cv>
2025-08-31 20:45:55 +0300

    feat(first-steps): complete the "JavaScript specials" section
    General recap of the "JavaScript Fundamentals" chapter.
    
    Signed-off-by: Efe Özyay <gh@efe.cv>

---
+++ first-steps/18-javascript_specials/javascript-specials.js
@@ -0,0 +1,107 @@
+// Code structure
+alert("Hello"); alert("World");
+
+alert("Hello")
+alert("World")
+
+alert("There will be an error after this message")
+
+[(1, 2)].forEach(alert);
+
+// function f() {
+//     // no semicolon needed after function declaration
+// }
+
+// for (;;) {
+//     // no semicolon needed after the loop
+// }
+
+// Strict mode
+// To fully enable all features of modern JavaScript, we should start scripts
+// with "use strict".
+
+// Variables
+let x = 5;
+x = "John";
+
+typeof null == "object"; // error in the language
+typeof function () {} == "function"; // functions are treated specially
+
+// Interaction
+let userName = prompt("Your name?", "Alice");
+let isTeaWanted = confirm("Do you want some tea?");
+
+alert("Visitor: " + userName); // Alice
+alert("Tea wanted: " + isTeaWanted); // true
+
+// Operators
+alert("1" + 2); // '12', string
+alert(1 + "2"); // '12', string
+
+alert(0 == false); // true
+alert(0 == ""); // true
+
+// Loops
+// 1
+// while (condition) {
+//   ...
+// }
+
+// 2
+// do {
+//   ...
+// } while (condition);
+
+// 3
+// for(let i = 0; i < 10; i++) {
+//   ...
+// }
+
+// The "switch" construct
+// The "switch" construct can replace multiple if checks. It uses
+// === (strict equality) for comparisons.
+let age = prompt("Your age?", 18);
+
+switch (age) {
+    case 18:
+        alert("Won't work"); // the result of prompt is a string, not a number
+        break;
+
+    case "18":
+        alert("This works!");
+        break;
+
+    default:
+        alert("Any value not equal to one above");
+}
+
+// Functions
+// Function Declaration: the function in the main code flow
+function sum(a, b) {
+    let result = a + b;
+
+    return result;
+}
+
+// Function Expression: the function in the context of an expression
+sum = function (a, b) {
+    let result = a + b;
+
+    return result;
+};
+
+// Arrow functions:
+// expression on the right side
+sum = (a, b) => a + b;
+
+// or multi-line syntax with { ... }, need return here:
+sum = (a, b) => {
+    // ...
+    return a + b;
+};
+
+// without arguments
+let sayHi = () => alert("Hello");
+
+// with a single argument
+let double = (n) => n * 2;
