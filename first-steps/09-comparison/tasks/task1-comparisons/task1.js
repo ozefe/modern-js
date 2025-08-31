5486030bab5877efdac86922e06c6cad36cd50d4
Efe Özyay <gh@efe.cv>
2025-08-31 12:10:05 +0300

    feat(first-steps): complete the "Comparisons" section
    Key topics included:
    - Comparison operators return a boolean value.
    - Strings are compared letter-by-letter in the "dictionary" (unicode)
    order.
    - When values of different types are compared, they get converted to
    numbers (with the exclusion of a strict equality check (`===`)).
    - The values `null` and `undefined` equal (`==`) each other and do not
    equal any other value.
    - Using comparisons like `>` or `<` with variables that can occasionally
    be `null` or `undefined` can lead to mistakes. Checking for `null` and
    `undefined` separately is a good idea.
    
    Tasks completed:
    - Comparisons: Showcasing how primitive comparisons work in JavaScript.
    
    Signed-off-by: Efe Özyay <gh@efe.cv>

---
+++ first-steps/09-comparison/tasks/task1-comparisons/task1.js
@@ -0,0 +1,7 @@
+alert(5 > 4); // true
+alert("apple" > "pineapple"); // "a" > "p" -> false
+alert("2" > "12"); // "2" > "1" -> true
+alert(undefined == null); // true
+alert(undefined === null); // false
+alert(null == "\n0\n"); // null only equals null and undefined, false
+alert(null === +"\n0\n"); // false
