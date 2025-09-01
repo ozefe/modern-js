26c5f3a905b65fabd302ace3ddb0fc430b51d0a8
Efe Özyay <gh@efe.cv>
2025-09-02 00:32:29 +0300

    style(first-steps): minor line break changes
    Signed-off-by: Efe Özyay <gh@efe.cv>

--- 01-first_steps/05-types/undefined.js
+++ 01-first_steps/05-types/undefined.js
@@ -2,7 +2,6 @@
 // The meaning of undefined is "value is not assigned". If a variable is
 // declared, but not assigned, then its value is undefined:
 let age;
-
 alert(age); // shows "undefined"
 
 // Technically, it is possible to explicitly assign undefined to a variable:
@@ -10,5 +9,4 @@ let age2 = 100;
 
 // change the value to undefined
 age2 = undefined;
-
 alert(age2); // "undefined"
