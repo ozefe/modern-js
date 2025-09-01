68b35df9460f747b7c7e899dc2962b5a94d6c84a
Efe Özyay <gh@efe.cv>
2025-09-02 00:57:15 +0300

    fix(first-steps): uncomment commented code
    Signed-off-by: Efe Özyay <gh@efe.cv>

--- 01-first_steps/15-function_basics/tasks/task2-rewrite_the_function_using_or/task2.js
+++ 01-first_steps/15-function_basics/tasks/task2-rewrite_the_function_using_or/task2.js
@@ -1,18 +1,18 @@
 let userAge = +prompt("Please enter your age: ", 19);
 
-// function checkAge(age) {
-//     if (age > 18) {
-//         return true;
-//     } else {
-//         return confirm("Did parents allow you?");
-//     }
-// }
-// alert(checkAge(userAge));
+function checkAge(age) {
+    if (age > 18) {
+        return true;
+    } else {
+        return confirm("Did parents allow you?");
+    }
+}
+alert(checkAge(userAge));
 
-// function checkAge(age) {
-//     return age > 18 ? true : confirm("Did parents allow you?");
-// }
-// alert(checkAge(userAge));
+function checkAge(age) {
+    return age > 18 ? true : confirm("Did parents allow you?");
+}
+alert(checkAge(userAge));
 
 function checkAge(age) {
     return age > 18 || confirm("Did parents allow you?");
